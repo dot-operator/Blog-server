ae8328233dbfe7aa38ddd2a4e07def958ee3d519
Sullivan Boyd <sullivan.h.boyd9@gmail.com>
2019-08-13 23:13:16 +0100

    Secured it.

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ def not_found(exc):
 
 def main():
     database.create_tables([Entry, FTSEntry])
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=443, debug=True, ssl_context=('../fullchain.pem', '../privkey.pem'))
 
 if __name__ == '__main__':
     main()
